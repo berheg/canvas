a75f9e2da2830f981d59ff7977726f8aa2aaf236
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-29 20:04:52 +0200

    for loop added for draw circle

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)'; 
 c.fillRect(300, 300, 100, 100);
 console.log(canvas);
 
@@ -23,3 +24,11 @@ c.beginPath();
 c.arc(300, 300, 30, 0, Math.PI*2, false);
 c.strokeStyle = 'blue';
 c.stroke();
+for(let i = 0; i < 100; i++){
+    let x =Math.random()*window.innerWidth;
+    let y = Math.random()* window.innerHeight;
+    c.beginPath();
+    c.arc(x, y, 30, 0, Math.PI*2, false);
+    c.strokeStyle = 'blue';
+    c.stroke();
+}
