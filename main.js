816b253c201d4557fc88891cbf743b5c984338a1
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-29 19:34:27 +0200

    draw line added

--- main.js
+++ main.js
@@ -7,8 +7,13 @@ c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 0, 255, 0.5)';
 c.fillRect(400, 100, 100, 100);
+c.fillRect(300, 300, 100, 100);
 console.log(canvas);
 
 //Draw line
-/*c.beginPath();
-c.moveTo()*/
+c.beginPath();
+c.moveTo(500, 300);
+c.lineTo(300,100);
+c.lineTo(400, 300);
+c.strokeStyle = '#fa34a3';
+c.stroke();
